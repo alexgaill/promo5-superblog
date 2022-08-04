f0f171944ffb27d588e9782ee2d59a3793406f4c
alexandre gaillot <gaillotalexandre@yahoo.fr>
2022-08-04 11:09:16 +0200

    mailer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,18 +4,22 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use Symfony\Component\Mailer\Mailer;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         $user = new User();
         $user->setRoles(['ROLE_ADMIN']);
@@ -34,8 +38,19 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            
+            $email = (new TemplatedEmail())
+                    ->from('no-reply@superblog.fr')
+                    ->to($user->getEmail())
+                    ->htmlTemplate('email/confirmation.html.twig')
+            ;
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+                $this->addFlash('danger', "L'email de confirmation n'a pas pu être envoyé. Erreur: ". $e->getMessage());
+            }
 
-            return $this->redirectToRoute('app_home');
+            // return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration/register.html.twig', [
